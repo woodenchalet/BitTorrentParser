22e6fe6efadb497d6d8e473ace19d6e3440c6f7c
Yi Luo <chalet056@gmail.com>
2018-08-26 15:44:27 -0400

    Update travis file

--- torrent_parser/client.py
+++ torrent_parser/client.py
@@ -4,8 +4,5 @@ from torrent_parser import TorrentParser
 
 path = sys.argv[1]
 
-# with TorrentParser(path) as parser:
-#     torrent = parser.output_torrent_object()
-
-parser = TorrentParser(path)
-torrent = parser.output_torrent_object()
+with TorrentParser(path) as parser:
+    torrent = parser.output_torrent_object()
