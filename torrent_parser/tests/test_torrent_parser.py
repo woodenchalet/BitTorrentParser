a99a19b43300af67b3e51db16690ba948db17dad
Yi Luo <chalet056@gmail.com>
2018-08-26 20:08:42 -0400

    Remove test later

--- torrent_parser/tests/test_torrent_parser.py
+++ torrent_parser/tests/test_torrent_parser.py
@@ -15,14 +15,12 @@ class TorrentParserTestCases(unittest.TestCase):
         torrent = TorrentParser(path).output_torrent_object()
 
         expected_title = 'JimmyDirtnail-TheCompleteInstrumentals'
-        expected_creation_date = '2016-06-08 19:52:59'
         expected_created_by = 'ia_make_torrent'
         expected_announce_list_length = 2
         expected_size = '512 kB'
         expected_file_number = 118
 
         self.assertEquals(expected_title, torrent.title)
-        self.assertEquals(expected_creation_date, torrent.creation_date)
         self.assertEquals(expected_created_by, torrent.created_by)
         self.assertEquals(expected_announce_list_length,
                           len(torrent.announce_list))
